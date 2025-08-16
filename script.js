dbefb97d5cb9a85e2e5fa23674b50b07b407987d
siddhant2895 <siddhant2895@gmail.com>
2025-08-17 01:37:41 +0200

    Reworked v1.1 - Hamburger menu added for phones

--- script.js
+++ script.js
@@ -20,3 +20,19 @@ const observer = new IntersectionObserver((entries) => {
 document.querySelectorAll("section").forEach((section) => {
   observer.observe(section);
 });
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navLinks.classList.toggle("active");
+});
+
+// Close menu when clicking a link
+document.querySelectorAll(".nav-links a").forEach((link) =>
+  link.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navLinks.classList.remove("active");
+  })
+);
